20c692f77db188e332f7d3ff1c4dce5becb16a40
skydev2020 <skydev2020@outlook.com>
2020-07-19 10:21:12 -0400

    Show Phones

--- wp-content/themes/shapely-child/page-templates/cellable-home.php
+++ wp-content/themes/shapely-child/page-templates/cellable-home.php
@@ -17,26 +17,17 @@ get_header();
 				the_content();
 	
 			endwhile; // End of the loop.
+			$phones = $wpdb->get_results("SELECT * FROM wp_cellable_phones", ARRAY_A);
 			?>
 			<div class="text-center">
+				<?php foreach ($phones as $phone): ?>
 				<div class="col-sm-4 text-center">
 					<a class="btn btn-default" href="/Phones/Carriers/1" style="font-size: 50px; height: 350px; left: 58px; width: 250px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin-top: 50px; ">
-						<img src="https://cellableimages.blob.core.windows.net/systemimages/apple.png" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
-						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;">Apple</p>
+						<img src="<?= get_stylesheet_directory_uri()?>/assets/images/<?= $phone['image_file'] ?>" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
+						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;"><?= $phone['brand'] ?></p>
 					</a>
 				</div>
-				<div class="col-sm-4 text-center">
-					<a class="btn btn-default" href="/Phones/Carriers/1" style="font-size: 50px; height: 350px; left: 58px; width: 250px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin-top: 50px; ">
-						<img src="https://cellableimages.blob.core.windows.net/systemimages/apple.png" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
-						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;">Apple</p>
-					</a>
-				</div>
-				<div class="col-sm-4 text-center">
-					<a class="btn btn-default" href="/Phones/Carriers/1" style="font-size: 50px; height: 350px; left: 58px; width: 250px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin-top: 50px; ">
-						<img src="https://cellableimages.blob.core.windows.net/systemimages/apple.png" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
-						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;">Apple</p>
-					</a>
-				</div>
+				<?php endforeach; ?>
 			</div>
 		</div><!-- #primary -->
 	</div>
