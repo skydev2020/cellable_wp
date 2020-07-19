90d184092772ad174f43b6f348a991119376fdfa
skydev2020 <skydev2020@outlook.com>
2020-07-19 09:45:26 -0400

    Homepage Carrier

--- wp-content/themes/shapely-child/page-templates/cellable-home.php
+++ wp-content/themes/shapely-child/page-templates/cellable-home.php
@@ -10,6 +10,34 @@ get_header();
 	<div class="row">
 		<div id="primary" class="col-md-12 mb-xs-24 <?php echo esc_attr( $layout_class ); ?>">
 			
+			<?php
+			while ( have_posts() ) : the_post();
+
+				// get_template_part( 'template-parts/content' );
+				the_content();
+	
+			endwhile; // End of the loop.
+			?>
+			<div class="text-center">
+				<div class="col-sm-4 text-center">
+					<a class="btn btn-default" href="/Phones/Carriers/1" style="font-size: 50px; height: 350px; left: 58px; width: 250px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin-top: 50px; ">
+						<img src="https://cellableimages.blob.core.windows.net/systemimages/apple.png" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
+						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;">Apple</p>
+					</a>
+				</div>
+				<div class="col-sm-4 text-center">
+					<a class="btn btn-default" href="/Phones/Carriers/1" style="font-size: 50px; height: 350px; left: 58px; width: 250px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin-top: 50px; ">
+						<img src="https://cellableimages.blob.core.windows.net/systemimages/apple.png" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
+						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;">Apple</p>
+					</a>
+				</div>
+				<div class="col-sm-4 text-center">
+					<a class="btn btn-default" href="/Phones/Carriers/1" style="font-size: 50px; height: 350px; left: 58px; width: 250px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin-top: 50px; ">
+						<img src="https://cellableimages.blob.core.windows.net/systemimages/apple.png" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
+						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;">Apple</p>
+					</a>
+				</div>
+			</div>
 		</div><!-- #primary -->
 	</div>
 <?php
