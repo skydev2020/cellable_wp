93f16f85676d519e239788680dffcb0e2234fe13
skydev2020 <skydev2020@outlook.com>
2020-07-19 08:46:15 -0400

    Add header and home templates

---
+++ wp-content/themes/shapely-child/page-templates/cellable-home.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Cellable Homepage
+Template Post Type: page
+*/
+get_header(); 
+?>
+
+<?php $layout_class = shapely_get_layout_class(); ?>
+	<div class="row">
+		<div id="primary" class="col-md-12 mb-xs-24 <?php echo esc_attr( $layout_class ); ?>">
+			
+		</div><!-- #primary -->
+	</div>
+<?php
+get_footer();
