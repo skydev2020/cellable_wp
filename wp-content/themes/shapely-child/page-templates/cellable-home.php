521d95d5bc3df80034f6dc02e7db96c5f8d61727
skydev2020 <skydev2020@outlook.com>
2020-07-19 10:43:48 -0400

    home phone update

--- wp-content/themes/shapely-child/page-templates/cellable-home.php
+++ wp-content/themes/shapely-child/page-templates/cellable-home.php
@@ -8,7 +8,7 @@ get_header();
 
 <?php $layout_class = shapely_get_layout_class(); ?>
 	<div class="row">
-		<div id="primary" class="col-md-12 mb-xs-24 <?php echo esc_attr( $layout_class ); ?>">
+		<div id="primary" class="col-md-12 mb-xs-24 homepage">
 			
 			<?php
 			while ( have_posts() ) : the_post();
@@ -21,10 +21,10 @@ get_header();
 			?>
 			<div class="text-center">
 				<?php foreach ($phones as $phone): ?>
-				<div class="col-sm-4 text-center">
-					<a class="btn btn-default" href="/Phones/Carriers/1" style="font-size: 50px; height: 350px; left: 58px; width: 250px; padding: 10px; border-radius: 5px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); margin-top: 50px; ">
-						<img src="<?= get_stylesheet_directory_uri()?>/assets/images/<?= $phone['image_file'] ?>" style="width: 185px; height: 224px; left: 200px; margin-top: 27px; padding-top: 10px;">
-						<p style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; text-align: center; margin-right: 50px;"><?= $phone['brand'] ?></p>
+				<div class="col-sm-4 text-center phone">
+					<a class="btn btn-default" href="/Phones/Carriers/1">
+						<img src="<?= get_stylesheet_directory_uri()?>/assets/images/<?= $phone['image_file'] ?>" style="width: 185px; height: 224px; margin-top: 27px; padding-top: 10px;">
+						<p class="text-center" style="font-family: 'HP Simplified'; padding-top: 50px; font-size: 25px; width: 249px; height: 70px; color: #000; margin-right: 50px;"><?= $phone['brand'] ?></p>
 					</a>
 				</div>
 				<?php endforeach; ?>
